b2a7c8c2217caa4a801dbb113cc0edf144a4fc71
ShaneC <122194410+ShaneC101@users.noreply.github.com>
2023-05-03 19:12:58 +1000

    modal hover and email

--- Final Project/index.js
+++ Final Project/index.js
@@ -1,12 +1,12 @@
 // 
 // 
-// 1sgM01LfXCb0-RvSF
+// 
 
 function contact(event) {
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading');
     const success = document.querySelector('.modal__overlay--success');
-    loading.classlist += " modal__overlay--visable";
+    loading.classList += " modal__overlay--visable";
     emailjs
     .sendForm(
      'service_o7pv847',
@@ -14,12 +14,22 @@ function contact(event) {
      event.target,
      '1sgM01LfXCb0-RvSF'
     ).then(() => {
-        loading.classlist.removel{"modal__overlay--visable"};
-        success.classlist += " modal__overlay--visable";
+        loading.classList.removel(" modal__overlay--visable");
+        success.classList += " modal__overlay--visable";
     }).catch(() => {
         loading.classList.removel("modal__overlay--visable");
         alert(
             "the email service is temporarily unavailable. Please contact me direct at wecholders@gmial.com"
-        )
+        );
     })
+}
+
+let isModalOpen = false;
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove('model--open');
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
 }
