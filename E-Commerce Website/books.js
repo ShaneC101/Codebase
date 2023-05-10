4572b58d79cdaf792866b1f8366c2d8ee71383ef
ShaneC <122194410+ShaneC101@users.noreply.github.com>
2023-05-10 17:34:00 +1000

    javascript arrays

--- E-Commerce Website/books.js
+++ E-Commerce Website/books.js
@@ -1,32 +1,51 @@
-function renderBooks() {
-    const booksWrapper = document.querySelector('.books');
+function renderBooks(filter) {
+    const booksWrapper = document.querySelector(".books");
     
     const books = getBooks();
-    console.log(books)
-    booksWrapper.innerHTML =
-    `<div class="book">
-    <figure class="book__img--wrapper">
-      <img class="book__img" src="./assets/deep work.jpeg" alt="">
-    </figure>
-    <div class="book__title">
-      Deep Work
-    </div>
-    <div class="book__ratings">
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star-half-alt"></i>
-    </div>
-    <div class="book__price">
-      <span class="book__price--normal">$59.95</span> $14.95
-    </div>
-  </div>`
+    
+   if (filter ==='LOW_TO_HIGH') {
+    books.sort((a, b) => a.originalPrice - b.originalPrice);
+   }
+   else if (filter ==='HIGH_TO_LOW') {
+    books.sort((a, b) => b.originalPrice - a.originalPrice);
+   }
+    else if (filter ==='RATING') {
+      books.sort((a, b) => b.rating - a.rating);
+   }
+
+    const booksHtml = books.map((book) => {
+      return `<div class="book">
+      <figure class="book__img--wrapper">
+        <img class="book__img" src="${book.url}" alt="">
+      </figure>
+      <div class="book__title">
+        ${book.title}
+      </div>
+      <div class="book__ratings">
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star"></i>
+        <i class="fas fa-star-half-alt"></i>
+      </div>
+      <div class="book__price">
+        <span>$${book.originalPrice.toFixed(2)}</span>
+      </div>
+    </div>`;
+    })
+    .join("");
+
+    booksWrapper.innerHTML = booksHtml;
 }
+ 
+function filterBooks(event) {
+renderBooks(event.target.value);
+ }
+
 
 setTimeout(() => {
-renderBooks();
-},);
+  renderBooks();
+});
 // FAKE DATA
 function getBooks() {
     return [
