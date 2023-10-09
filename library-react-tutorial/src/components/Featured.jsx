6927ac311b068c160317eb7ebb05d611d7eb9b83
ShaneC <122194410+ShaneC101@users.noreply.github.com>
2023-10-10 07:22:41 +1000

    books upload

--- library-react-tutorial/src/components/Featured.jsx
+++ library-react-tutorial/src/components/Featured.jsx
@@ -1,7 +1,9 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import Book from './ui/Book';
+import { books } from '../data';
 
 const Featured = () => {
+  console.log(books);
  return (
    <section id="features">
     <div className="container">
@@ -10,29 +12,10 @@ const Featured = () => {
             Featured <span className="purple">Books</span>
             </h2>
             <div className="books">
-                <div className="book">
-                    <a href="">
-                     <figure className="book__img--wrapper">
-                       <img src="https://covers.openlibrary.org/b/id/10958382-L.jpg" alt="" className="book__img" />
-                      </figure>
-                </a>
-                <div className="book__title">
-                    <a href="/" className="book__title--link">
-                        Atomic Habbits
-                    </a>
-                </div>
-                <div className="book__ratings">
-                  <FontAwesomeIcon icon="star" />
-                  <FontAwesomeIcon icon="star" />
-                  <FontAwesomeIcon icon="star" />
-                  <FontAwesomeIcon icon="star" />
-                  <FontAwesomeIcon icon="star-half-alt" />
-                </div>
-                <div className="book__price">
-                    <span className="book__price--normal">$15.00</span>
-                    $10.00
-                </div>
-                </div>
+                <Book />
+                <Book />
+                <Book />
+                <Book />
             </div>
          </div>
         </div>
