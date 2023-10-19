d9248d0b7f8b41baef2a657b62376a272f9c2467
ShaneC <122194410+ShaneC101@users.noreply.github.com>
2023-10-19 14:27:25 +1000

    app complete

--- library-react-tutorial/src/components/Book.jsx
+++ library-react-tutorial/src/components/Book.jsx
@@ -1,30 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Rating from "./Rating";
 import { Link } from "react-router-dom";
 import Price from "../components/Price";
 
 const Book = ({ book }) => {
-  const [img, setImg] = useState();
-
-  useEffect(() => {
-    const image = new Image();
-    image.src = book.url;
-    image.onLoad = () => {
-      setTimeout(() => {
-        setImg(image);
-      }, 300);
-     }
-  })
-
-  return (
+ return (
     <div className="book">
-      {img ? (
-        <>
           <Link to={`/books/${book.id}`}>
             <figure className="book__img--wrapper">
               <img 
-                src={img.src} 
+                src={book.url} 
                 alt="" 
                 className="book__img"
                />
@@ -36,21 +22,9 @@ const Book = ({ book }) => {
             </Link>
           </div>
           <Rating rating={book.rating} />
-          <Price
-            salePrice={book.saleprice}
-            originalPrice={book.originalPrice}
-          />
-        </>
-      ) : (
-        <>
-          <div className="book__img--skeleton"></div>
-          <div className="skeleton book__title--skeleton"></div>
-          <div className="skeleton book__rating--skeleton"></div>
-          <div className="skeleton book__price--skeleton"></div>
-        </>
-      )}
-    </div>
-  );
-};
+          <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
+          </div>
+      );
+  };
 
 export default Book;
